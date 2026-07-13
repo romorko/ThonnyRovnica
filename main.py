373793d84108acc1afdc87e8f3f10d10ef199ce4
roman <romorko@proton.me>
2026-07-13 19:35:04 +0200

    Zakladna verzia c.1

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-samohlasky:list = [i for i in "veta je prva"]
-print(samohlasky)
+# Projekt rovnica na výpočet koreňa lineárnej rovnice
+print("Zadaj koeficienkty lineárnej rovnice: ax + b=0")
+a: float = float(input("Koeficient a = "))
+b: float = float(input("Koeficient b = "))
+print(f"Rovnica ma tvar: {a}x + {b} = 0")
+koren:float = -b/a
+print(f'Koren rovnice x={koren:.2f}')
